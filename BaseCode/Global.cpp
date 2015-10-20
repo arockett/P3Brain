6aaadbf68fe92f6192995f7438229420ad27936b
cliff-bohm <cliff.bohm@gmail.com>
2015-10-20 18:06:02 -0400

    adding global.*

---
+++ BaseCode/Global.cpp
@@ -0,0 +1,22 @@
+
+#include "Global.h"
+#include "../Utilities/Parameters.h"
+
+//global variables that should be accessible to all
+int Global::update = 0;
+set<int> Global::inUseGateTypes;
+
+int& Global::repNumber = Parameters::register_parameter("repNumber", 101, "Replicate ID and seed (if seedWithPID not set true)", "GLOBAL");
+bool& Global::seedRandom = Parameters::register_parameter("seedRandom", false,
+		"if seedRandom, random number generator will seeded randomly", "GLOBAL");
+int& Global::updates = Parameters::register_parameter("updates", 200, "how long the program will run", "GLOBAL");
+int& Global::popSize = Parameters::register_parameter("popSize", 100, "number of genomes in the populaiton", "GLOBAL");
+int& Global::dataInterval = Parameters::register_parameter("dataInterval", 1, "How often to write to data file","DATA");
+int& Global::genomeInterval = Parameters::register_parameter("genomeInterval", 1, "How often to write genome file","DATA");
+int& Global::pruneInterval = Parameters::register_parameter("pruneInterval", dataInterval, "How often to write to data file","DATA");
+string& Global::DataFileName = Parameters::register_parameter("dataFileName", (string)"data.csv",
+		"name of genome file (stores genomes for line of decent)","DATA");
+string& Global::GenomeFileName = Parameters::register_parameter("genomeFileName", (string)"genome.csv",
+		"name of data file (stores data i.e. scores","DATA");
+int& Global::bitsPerBrainAddress = Parameters::register_parameter("bitsPerBrainAddress", 10,
+		"how many bits are evaluated to determine the brain addresses", "AGENT");
---
+++ BaseCode/Global.h
@@ -0,0 +1,43 @@
+//
+//  Global.h
+//  BasicMarkovBrainTemplate
+//
+//  Created by Arend Hintze on 5/31/15.
+//  Copyright (c) 2015 Arend Hintze. All rights reserved.
+//
+
+#ifndef __BasicMarkovBrainTemplate__Global_
+#define __BasicMarkovBrainTemplate__Global_
+
+#include <map>
+#include <memory>
+#include <set>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+//#include "Genome.h"
+
+using namespace std;
+
+class Global {
+public:
+	// parameters
+	static int& repNumber; // number used for output directory names and random seed
+	static bool& seedRandom; // if this is true, seed with a random value
+	static int& updates; // run until there is a MCRA at this time
+	static int& popSize; // number of genomes in the population
+	static int& dataInterval; // how often to write out data
+	static int& genomeInterval; // how often to write out genomes
+	static int& pruneInterval; // how often to attempt to prune the LOD
+	static string& DataFileName; // name of the Data file
+	static string& GenomeFileName; // name of the Genome file (genomes on LOD)
+	static int& bitsPerBrainAddress; // how many bits are evaluated to determine the brain addresses.
+									 // i.e. ceil(bitsPerBrainAddress/8) = number of genome sites used
+
+	// non configurable values (not set directly by config methods)
+	static int update;  // the current "time"
+	static set<int> inUseGateTypes; // this is global - a list of the start codeons for gates being used
+};
+
+#endif /* defined(__BasicMarkovBrainTemplate__Global__) */
