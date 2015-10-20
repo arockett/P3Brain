8d50cf3371db1465947b1a2fd9419b7eaf70f4c9
Aaron Beckett <becketta@msu.edu>
2015-10-19 23:59:50 -0400

    Add BitGate

---
+++ P3Brain/BitGate.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+* file: BitAgent.cpp
+*
+* author: Aaron Beckett
+* date: 10/15/2015
+******************************************************************************/
+
+#include "BitGate.h"
+
+
+BitGate::BitGate( const vector<int>& ins, int id, const vector<bool>& logic ) :
+    mGateIndex(id),
+    mLogic(logic)
+{
+    I = ins;
+    O.clear();
+    O.push_back( mGateIndex );
+}
+
+
+BitGate::~BitGate()
+{
+}
+
+
+void BitGate::update( double *states, double *nextStates )
+{
+    int outIndex = 0;
+    if( Bit( states[I[0]] ) )
+    {
+        outIndex += 2;
+    }
+    if( Bit( states[I[1]] ) )
+    {
+        ++outIndex;
+    }
+
+    nextStates[mGateIndex] = Bit( mLogic[outIndex] );
+}
---
+++ P3Brain/BitGate.h
@@ -0,0 +1,24 @@
+#pragma once
+/******************************************************************************
+* file: BitAgent.h
+*
+* author: Aaron Beckett
+* date: 10/15/2015
+******************************************************************************/
+
+#include "Gate.h"
+
+class BitGate : public Gate
+{
+public:
+    BitGate() = delete;
+    BitGate(const vector<int>& ins, int out, const vector<bool>& logic);
+    virtual ~BitGate();
+
+    void update( double *states, double *nextStates ) override;
+
+private:
+    int mGateIndex;
+    vector<bool> mLogic;
+};
+
