f6eb1bb834c8f0c8731edc6ad0dc0728b6018f7b
cliff-bohm <cliff.bohm@gmail.com>
2015-11-02 15:51:35 -0500

    changed loopMod

--- Utilities/Utilities.h
+++ Utilities/Utilities.h
@@ -16,11 +16,9 @@ using namespace std;
 /*
  * return x % y were (-1 % y) = (y - 1)
  */
-inline int loopMod(int x, int y) {
-	x += (y * (abs(x / y) + 1)); // this adds a number of 'y's to 'x' so as to insure that 'x' is greater then 0. the numer of 'y's is not important.
-	return x % y;
+int loopMod(const int numerator, const int denominator) {
+  return ((numerator % denominator) + denominator) % denominator;
 }
-
 /*
  * returns 1 if "d" is greater than 0, else return 0
  */
