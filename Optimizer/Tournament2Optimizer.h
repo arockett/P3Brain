7ce13c942815b6ca1afcd45013c776974eb7f0d4
cliff-bohm <cliff.bohm@gmail.com>
2016-02-24 12:52:04 -0500

    chaged names of optimizers to make them standard

---
+++ Optimizer/Tournament2Optimizer.h
@@ -0,0 +1,19 @@
+//
+//  Optimizer.h
+//  BasicMarkovBrainTemplate
+//
+//  Created by Arend Hintze on 5/29/15.
+//  Copyright (c) 2015 Arend Hintze. All rights reserved.
+//
+
+#ifndef __BasicMarkovBrainTemplate__Tournament2_Optimizer__
+#define __BasicMarkovBrainTemplate__Tournament2_Optimizer__
+
+#include "Optimizer.h"
+
+class Tournament2Optimizer : public BaseOptimizer {
+ public:
+	virtual void makeNextGeneration(vector<shared_ptr<Organism>> &population);
+};
+
+#endif /* defined(__BasicMarkovBrainTemplate__Tournament2_Optimizer__) */
